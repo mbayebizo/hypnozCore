a223b69666823e8306c8bfd391a022d1ca7c6412
Mbaye Babacar SARR <mbsdev2018@gmail.com>
2022-09-09 13:17:40 +0200

    init config ci

--- src/test/java/net/hypnozcore/hypnozcore/HypnozCoreApplicationTests.java
+++ src/test/java/net/hypnozcore/hypnozcore/HypnozCoreApplicationTests.java
@@ -3,11 +3,16 @@ package net.hypnozcore.hypnozcore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class HypnozCoreApplicationTests {
 
     @Test
     void contextLoads() {
+        String actual="test";
+        String expected="test";
+        assertThat(actual).as("assertion desc").isEqualTo(expected);
     }
 
 }
