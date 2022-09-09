e6f169cae89ef91294fd8ff08202bb58730b0484
Mbaye Babacar SARR <mbsdev2018@gmail.com>
2022-09-09 08:51:31 +0200

    Initial commit

---
+++ src/main/java/net/hypnozcore/hypnozcore/HypnozCoreApplication.java
@@ -0,0 +1,13 @@
+package net.hypnozcore.hypnozcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypnozCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypnozCoreApplication.class, args);
+	}
+
+}
